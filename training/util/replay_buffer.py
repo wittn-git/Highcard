c42300489f3decfde7b1585a0191bfc7c1f65e17
wittn-git <wittner.dominic@gmail.com>
2025-09-04 19:04:50 +0200

    Added replaybuffer

---
+++ training/util/replay_buffer.py
@@ -0,0 +1,17 @@
+from util.classes import Card, State
+
+import random
+
+class ReplayBuffer:
+
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.buffer = []
+
+    def push(self, state : State, action : Card, reward : float, next_state : State):
+        while len(self.buffer) >= self.capacity:
+            self.buffer.pop(0)
+        self.buffer.append((state, action, reward, next_state))
+
+    def sample(self, batch_size: int):
+        return random.sample(self.buffer, batch_size)
